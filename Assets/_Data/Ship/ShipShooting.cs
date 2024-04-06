3b257c0ff1042a52cb103bffab99924aa35b6446
lephuchung <lephuchung003@gmail.com>
2024-04-07 03:22:20 +0700

    e9

--- Assets/_Data/Ship/ShipShooting.cs
+++ Assets/_Data/Ship/ShipShooting.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class ShipShooting : MonoBehaviour
 {
     [SerializeField] protected bool isShooting = false;
+    [SerializeField] protected float shootDelay = 1f;
+    [SerializeField] protected float shootTimer = 0f;
     [SerializeField] protected Transform bulletPrefab;
 
     void Update()
     {
         this.IsShooting();
+    }
+
+    private void FixedUpdate()
+    {
         this.Shooting();
     }
 
     protected virtual void Shooting()
     {
+        this.shootTimer += Time.fixedDeltaTime;
         if (!this.isShooting) return;
+
+        if (this.shootTimer < shootDelay) return;
+        this.shootTimer = 0;
 
         Vector3 spawnPos = transform.position;
         Quaternion rotation = transform.parent.rotation;
