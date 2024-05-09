fa8153f8287e6c32b370a7c961abdac29c55efe4
lephuchung <lephuchung003@gmail.com>
2024-05-10 00:28:08 +0700

    e23

--- Assets/_Data/Bullet/BulletCtrl.cs
+++ Assets/_Data/Bullet/BulletCtrl.cs
@@ -7,10 +7,14 @@ public class BulletCtrl : MyMonoBehaviour
     [SerializeField] protected DamageSender damageSender;
     public DamageSender DamageSender { get => damageSender; }
 
+    [SerializeField] protected BulletDespawn bulletDespawn;
+    public BulletDespawn BulletDespawn { get => bulletDespawn; }
+
     protected override void LoadComponents()
     {
         base.LoadComponents();
         this.LoadDamageSender();
+        this.LoadBulletDespawn();
     }
 
     protected virtual void LoadDamageSender()
@@ -18,5 +22,12 @@ public class BulletCtrl : MyMonoBehaviour
         if (this.damageSender != null) return;
         this.damageSender = transform.GetComponentInChildren<DamageSender>();
         Debug.Log(transform.name + ": LoadDamageSender", gameObject);
+    }
+
+    protected virtual void LoadBulletDespawn()
+    {
+        if (this.bulletDespawn != null) return;
+        this.bulletDespawn = transform.GetComponentInChildren<BulletDespawn>();
+        Debug.Log(transform.name + ": LoadBulletDespawn", gameObject);
     }
 }
