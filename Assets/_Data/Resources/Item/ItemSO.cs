f5ca0d1fca3d42dbd671aa3a299a959167269274
lephuchung <lephuchung003@gmail.com>
2024-09-08 20:18:13 +0700

    e34

--- Assets/_Data/Resources/Item/ItemSO.cs
+++ Assets/_Data/Resources/Item/ItemSO.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Item", menuName = "SO/Item")]
 public class ItemSO : ScriptableObject
 {
-    public string itemName = "Item";
     public ItemCode itemCode = ItemCode.NoItem;
+    public string itemName = "Item";
 }
