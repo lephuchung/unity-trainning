0f9e7a749f370010013bc9e6ddcc024a799ab7b3
lephuchung <lephuchung003@gmail.com>
2024-09-08 23:30:54 +0700

    e35

--- Assets/_Data/Damage/DamageSender.cs
+++ Assets/_Data/Damage/DamageSender.cs
@@ -8,12 +8,26 @@ public class DamageSender : MyMonoBehaviour
     {
         DamageReceiver damageReceiver = obj.GetComponentInChildren<DamageReceiver>();
         if (damageReceiver == null) return;
-        Debug.Log("sendmg");
         this.Send(damageReceiver);
+        this.CreateImpactFX();
     }
 
     public virtual void Send(DamageReceiver damageReceiver)
     {
         damageReceiver.Deduct(this.damage);
+    }
+
+    protected virtual void CreateImpactFX()
+    {
+        string fxName = this.GetImpactFx();
+
+        Vector3 hitPosition = transform.position;
+        Quaternion hitRotation = transform.rotation;
+        Transform fxImpact = FXSpawner.Instance.Spawn(fxName, hitPosition, hitRotation);
+        fxImpact.gameObject.SetActive(true);
+    }
+    protected virtual string GetImpactFx()
+    {
+        return FXSpawner.impact1;
     }
 }
---
+++ Assets/_Data/Item/Inventory/ItemPickupable.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class ItemPickupable : MyMonoBehaviour
+{
+    [SerializeField] protected SphereCollider _collider;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadTrigger();
+    }
+
+    protected virtual void LoadTrigger()
+    {
+        if (this._collider != null) return;
+        this._collider = transform.GetComponent<SphereCollider>();
+        this._collider.isTrigger = true;
+        this._collider.radius = 0.1f;
+        Debug.LogWarning(transform.name + " LoadTrigger", gameObject);
+    }
+}
