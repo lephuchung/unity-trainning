9cc2f9a2cb7b396d5ba24c6c542d3d4beb922663
lephuchung <lephuchung003@gmail.com>
2024-09-23 00:20:08 +0700

    e44

---
+++ Assets/_Data/Item/Inventory/ItemInventoryDrop.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+public class ItemInventoryDrop : InventoryAbstract
+{
+    protected override void Start()
+    {
+        base.Start();
+        Invoke(nameof(this.Test), 5);
+    }
+    protected virtual void Test()
+    {
+        this.DropItemIndex(0);
+    }
+    protected virtual void DropItemIndex(int itemIndex)
+    {
+        ItemInventory itemInventory = this.inventory.Items[itemIndex];
+        Debug.Log(itemInventory.itemProfile.itemCode);
+        Debug.Log(itemInventory.upgradeLevel);
+        Vector3 dropPos = transform.position;
+        dropPos.x += 1;
+        ItemDropSpawner.Instance.Drop(itemInventory, dropPos, transform.rotation);
+    }
+}
--- Assets/_Data/Item/ItemDropSpawner.cs
+++ Assets/_Data/Item/ItemDropSpawner.cs
@@ -1,11 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDropSpawner : Spawner
 {
     private static ItemDropSpawner instance;
-    
+
     public static ItemDropSpawner Instance => instance;
 
     protected override void Awake()
@@ -15,11 +14,20 @@ public class ItemDropSpawner : Spawner
         ItemDropSpawner.instance = this;
     }
 
-    public virtual void Drop (List<DropRate> dropList, Vector3 pos, Quaternion rot)
+    public virtual void Drop(List<DropRate> dropList, Vector3 pos, Quaternion rot)
     {
         ItemCode itemCode = dropList[0].itemSO.itemCode;
         Transform itemDrop = this.Spawn(itemCode.ToString(), pos, rot);
         if (itemDrop == null) return;
         itemDrop.gameObject.SetActive(true);
+    }
+
+    public virtual Transform Drop(ItemInventory itemInventory, Vector3 pos, Quaternion rot)
+    {
+        ItemCode itemCode = itemInventory.itemProfile.itemCode;
+        Transform itemDrop = this.Spawn(itemCode.ToString(), pos, rot);
+        if (itemDrop == null) return null;
+        itemDrop.gameObject.SetActive(true);
+        return itemDrop;
     }
 }
