9a463c977a01be45c9f607982ecb4c459a1b3a10
lephuchung <lephuchung003@gmail.com>
2024-09-24 00:14:39 +0700

    e45

--- Assets/_Data/Item/ItemCtrl.cs
+++ Assets/_Data/Item/ItemCtrl.cs
@@ -5,10 +5,14 @@ public class ItemCtrl : MyMonoBehaviour
     [SerializeField] protected ItemDespawn itemDespawn;
     public ItemDespawn ItemDespawn => itemDespawn;
 
+    [SerializeField] protected ItemInventory itemInventory;
+    public ItemInventory ItemInventory => itemInventory;
+
     protected override void LoadComponents()
     {
         base.LoadComponents();
         this.LoadItemDespawn();
+        this.LoadItemInventory();
     }
 
     protected virtual void LoadItemDespawn()
@@ -16,5 +20,20 @@ public class ItemCtrl : MyMonoBehaviour
         if (this.itemDespawn != null) return;
         this.itemDespawn = transform.GetComponentInChildren<ItemDespawn>();
         Debug.Log(transform.name + " LoadItemDespawn", gameObject);
+    }
+
+    public virtual void SetItemInventory(ItemInventory itemInventory)
+    {
+        this.itemInventory = itemInventory;
+    }
+
+    protected virtual void LoadItemInventory()
+    {
+        if (this.itemInventory != null) return;
+        ItemCode itemCode = ItemCodeParse.FromString(transform.name);
+        ItemProfileSO itemProfile = ItemProfileSO.FindByCode(itemCode);
+        this.itemInventory.itemProfile = itemProfile;
+        this.itemInventory.itemCount = 1;
+        Debug.Log(transform.name + ": LoadItemInventory", gameObject);
     }
 }
